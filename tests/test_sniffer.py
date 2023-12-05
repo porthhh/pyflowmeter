e90cbc928cc41b2acb98f25892eb169f8ac6c012
David <davidrarchilla@gmail.com>
2023-12-05 16:35:52 +0100

    canged test_file

--- tests/test_sniffer.py
+++ tests/test_sniffer.py
@@ -2,8 +2,8 @@ from pyflowmeter.sniffer import create_sniffer
 
 sniffer = create_sniffer(
             # input_file='tests/pcap_files/pkt.UDP.null.pcapng',
-            # server_endpoint='http://127.0.0.1:5000/send_traffic',
-            to_csv=True,
+            server_endpoint='http://127.0.0.1:5000/send_traffic',
+            # to_csv=True,
             # output_file='./flows_test.csv',
             verbose=False,
             sending_interval=5
